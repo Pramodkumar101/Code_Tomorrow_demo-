38e47a8993f687da794bb62f77a2fd045074b14b
Pramodkumar101 <pramod@xtranetindia.com>
2024-02-16 17:16:56 +0530

    add code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./components/Card";
+import Sidebar from "./components/Sidebar";
+
+
+export default function App() {
+  return (
+    <>
+    <Sidebar/>
+      <Card />
+    </>
+  );
+}
